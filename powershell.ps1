abdefa86f4c29d8f3b5fccfc5a652a44407d38b9
Joe Kampschmidt <jokecamp@gmail.com>
2013-11-19 11:16:49 -0500

    Updated powershell script

--- powershell.ps1
+++ powershell.ps1
@@ -1,5 +1,12 @@
 <#
     PowerShell template
+
+    Best Practices:
+        - use single quotes for plain strings (only use double when you need them)
+        - always return objects
+
+        http://windowsitpro.com/blog/my-12-powershell-best-practices
+        http://blogs.technet.com/b/heyscriptingguy/archive/2012/06/18/the-top-ten-powershell-best-practices-for-it-pros.aspx
 #>
 
 Function Test ([string] $p1, [string] $p2) {
@@ -9,4 +16,5 @@ Function Test ([string] $p1, [string] $p2) {
 }
 
 Test -p1 'hello' -p2 'World'
+
 
